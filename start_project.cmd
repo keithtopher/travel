c6abc32ab8c989ff88a67f49ab2fc6188701fb54
keithtopher <50483819+keithtopher@users.noreply.github.com>
2021-02-05 13:25:21 +0000

    Delete start_project.cmd
    Deleted from main branch, moved to gh-pages

--- start_project.cmd
+++
@@ -1,7 +0,0 @@
-set mypath=%cd%
-start cmd
-start "" "C:\Program Files\Git\git-bash.exe"
-start "" "C:\Program Files\Sublime Text 3\sublime_text.exe" "index.html"
-rem start . & rem open working directory in physical window
-rem start cmd /k jupyter lab clock_project.ipynb & rem start jupyter lab notebook
-rem start "" "C:\Users\keith\AppData\Local\Programs\MiKTeX 2.9\miktex\bin\x64\miktex-texworks.exe" C:\Users\keith\google_drive_school\Python\Mechanobiology\notes.tex & rem open TeX for notes
